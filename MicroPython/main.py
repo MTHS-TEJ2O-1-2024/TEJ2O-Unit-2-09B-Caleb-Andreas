e11c8548c0e77f8c49310348d8cd4e18f552afc5
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-09 21:07:42 +0000

    Made code that plays rock paper scissors

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,59 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from time import sleep
+import random
 
+# variables
+random_number = -1
+score_number = 0
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if accelerometer.was_gesture("shake"):
+        random_number = random.randint(0, 2)
+        display.clear()
+
+        # if random_number was 0 (rock)
+        if random_number == 0:
+            display.clear()
+            display.show(Image.SQUARE_SMALL)
+            sleep(5)
+            display.clear()
+            display.show(Image.HAPPY)
+
+        # if random_number was 1 (paper)
+        if random_number == 1:
+            display.clear()
+            display.show(Image.SQUARE)
+            sleep(5)
+            display.clear()
+            display.show(Image.HAPPY)
+
+        # if random_number was 2 (scissors)
+        if random_number == 2:
+            display.clear()
+            display.show(Image.SWORD)
+            sleep(5)
+            display.clear()
+            display.show(Image.HAPPY)
+
+# Adds 1 to score when a button is pressed.
+while True:
+    if button_a.is_pressed():
+        score_number = score_number + 1
+        display.clear()
+        display.show(Image.YES)
+        sleep(3)
+        display.clear
+        display.show(Image.HAPPY)
+
+# Display score when b button pressed.
+while True:
+    if button_.is_pressed():
+        display.clear()
+        display.scroll("Score:  " + str(score_number))
+        display.clear
+        display.show(Image.HAPPY)
