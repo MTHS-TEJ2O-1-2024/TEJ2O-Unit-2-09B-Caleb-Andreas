992298965f3aa3f47a8fe99e30ab3170b02bf16d
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-09 16:01:07 -0400

    Completed rock paper scissors program

--- main.ts
+++ main.ts
@@ -7,17 +7,20 @@
 
 // variables
 let randomNumber: number = 0
+let scoreNumber = 0
 
 randomNumber = -1
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
+// Runs rock paper scissors program when shaken.
 input.onGesture(Gesture.Shake, function () {
     randomNumber = randint(0, 2)
     basic.clearScreen()
 
     // if randomNumber was 0
     if (randomNumber == 0) {
+        basic.clearScreen()
         basic.showLeds(`
         . . . . .
         . # # # .
@@ -27,10 +30,11 @@ input.onGesture(Gesture.Shake, function () {
         `)
         basic.pause(5000)
         basic.showIcon(IconNames.Happy)
-        }
+    }
 
     // if randomNumber was 1
     if (randomNumber == 1) {
+        basic.clearScreen()
         basic.showIcon(IconNames.Square)
         basic.pause(5000)
         basic.showIcon(IconNames.Happy)
@@ -38,8 +42,26 @@ input.onGesture(Gesture.Shake, function () {
 
     // if randomNumber was 2
     if (randomNumber == 2) {
+        basic.clearScreen()
         basic.showIcon(IconNames.Scissors)
         basic.pause(5000)
         basic.showIcon(IconNames.Happy)
     }
+})
+
+// Add 1 to score when A button pressed.
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    scoreNumber = scoreNumber + 1
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+    basic.pause(3000)
+    basic.showIcon(IconNames.Happy)
+})
+
+// Show score when B button pressed.
+input.onButtonPressed(Button.B, function() {
+    basic.clearScreen()
+    basic.showString('Score: ' + (scoreNumber).toString())
+    basic.showIcon(IconNames.Happy)
 })
