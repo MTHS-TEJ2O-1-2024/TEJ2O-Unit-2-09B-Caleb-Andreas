68be96a32b2f8aa4d57eb0d44614eb17cf396a3e
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-10 14:53:31 +0000

    Fixed comment

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,7 +1,7 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Caleb Andreas
+Created on: Oct 2024
+This module is a program that plays rock paper scissors
 """
 
 from microbit import *
